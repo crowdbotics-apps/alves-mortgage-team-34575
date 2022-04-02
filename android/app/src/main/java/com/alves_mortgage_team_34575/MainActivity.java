966faec7a3d159c011eb55a968e5918a99f5d653
Crowdbotics <team@crowdbotics.com>
2022-04-02 16:47:36 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/alves_mortgage_team_34575/MainActivity.java
@@ -0,0 +1,15 @@
+package com.alves_mortgage_team_34575;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "alves_mortgage_team_34575";
+  }
+}
